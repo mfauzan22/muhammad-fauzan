332f1fcecb57d9113aa33120d1ee47f1f1662146
mfauzan22 <muhammadfauzan262004@gmail.com>
2026-02-11 19:25:02 +0700

    Add files via upload

---
+++ looping.php
@@ -0,0 +1,28 @@
+<?php
+$a= 1;
+echo "<select>";
+while ($a <= 31) {
+    echo "<option>$a</option>";
+    $a++;
+}
+echo "<select>";
+$bln= 1;
+do {
+    echo "<option>$bln</option>";
+    $bln++
+
+}
+
+while ($a <= 12) {
+    echo "<option>$a</option>";
+    $a++;
+}
+echo "<select>";
+$a= 2000;
+echo "<select>";
+while ($a <= 2030) {
+    echo "<option>$a</option>";
+    $a++;
+}
+echo "<select><br>";
+?>
